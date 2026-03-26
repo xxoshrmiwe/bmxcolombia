67dc874432875ee8b9eeea376c35bc7deba0b867
Oscar Clickpanda <oscargonzalez0710@gmail.com>
2026-03-26 16:30:21 -0500

    fix: use turbopack config for next 16

--- next.config.ts
+++ next.config.ts
@@ -2,18 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
-  outputFileTracingExcludes: {
-    '*': ['./node_modules/sanity/**', './node_modules/@sanity/**'],
-  },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      'sanity/structure': false,
-      'sanity': false,
-    };
-    return config;
-  },
+  turbopack: {},
 };
 
 export default nextConfig;
