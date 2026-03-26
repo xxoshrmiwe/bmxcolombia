f0e8c00b1b509c979825047610b605e2d2346141
Oscar Clickpanda <oscargonzalez0710@gmail.com>
2026-03-26 16:10:51 -0500

    fix: remove vision plugin from production config

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'sanity'
 import { structureTool } from 'sanity/structure'
-import { visionTool } from '@sanity/vision'
 import { schemaTypes } from './sanity/schemaTypes'
+
 export default defineConfig({
   name: 'bmxcolombia',
   title: 'BMX Colombia — Admin',
   basePath: '/studio',
   projectId: '0oez72ce',
   dataset: 'production',
-  plugins: [structureTool(), visionTool()],
+  plugins: [structureTool()],
   schema: { types: schemaTypes },
 })
